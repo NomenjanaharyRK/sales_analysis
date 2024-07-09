d3e36b8b83629ef9887e93fbe9bddf28e497b1f2
NomenjanaharyRK <ericnomenjanahary11@gmail.com>
2024-07-09 20:33:19 +0300

    chore: reinitialize project

--- scripts/correlation_analyzer.py
+++
@@ -1,7 +0,0 @@
-class CorrrelationAnalyzer:
-    def __init__(self, data):
-        self.data = data
-
-    def calculate_correlation(self):
-        correlation_matrix = self.data.corr()
-        return correlation_matrix
